0c4f3e54c0a29ec8b5b45945638e5850c306b5ea
heyprashant <prashant.p7804@gmail.com>
2020-12-19 19:33:56 +0530

    6-http

---
+++ 6-raw-http.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const url = 'http://api.weatherstack.com/current?access_key=b5b9c3b9e7791900c6f33020d52562bd&query=40,-75&units=m';
+
+const request = http.request(url, (response)=>{
+    let data ='';
+    
+    response.on('data', (chunk) =>{
+        data = data + chunk.toString();
+    });
+
+    response.on('end', ()=>{
+        const body = JSON.parse(data);
+        console.log(body);
+    });
+
+});
+
+request.on('error', (error)=>{
+    console.log('As error', error);
+});
+
+request.end();
